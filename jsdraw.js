fbdab9d061690360d26ca5da70f2d8dc8a3f095e
nanosonicf <nsonicnf@gmail.com>
2016-06-29 13:31:41 +0300

    index
    added first files

---
+++ jsdraw.js
@@ -0,0 +1,6 @@
+var c = document.getElementById("myFirstCanvas");
+var ctx = c.getContext("2d");
+//ctx.fillStyle = "#ff0000";
+//ctx.fillRect(0,0,150,750);
+ctx.font ="30px Arial";
+ctx.fillText("Hello js en htm",10,50);
